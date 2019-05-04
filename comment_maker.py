e319c8ffcd0775600831b70db376668392bc7874
pedro <pedromfgodinho@gmail.com>
2019-05-04 03:40:23 +0100

    Works for c now

--- comment_maker.py
+++ comment_maker.py
@@ -1,7 +1,7 @@
-
 max_len = int(input("Enter the max line len: "))
 
-print("Enter the text, ending with ENDCOMMENT on an empty line:")
+print("Enter the text, ending with 'ENDCOMMENT' on an empty line:")
+print("You can also write an empty line with 'SEPARATOR' for a full width separator.\n");
 
 text = ""
 
@@ -12,23 +12,26 @@ while inp != "ENDCOMMENT":
 
 text = text.split('\n')[:-1]
 
-print('*' * max_len)
+print('/' + '*' * (max_len-1))
 ind = 0
 while ind < len(text):
-    words = text[ind].split(' ')
+    if text[ind] == 'SEPARATOR':
+        print('*' * max_len)
+    else:
+        words = text[ind].split(' ')
 
-    line = ""
-    for word_i in range(len(words)):
-        if len(line) + len(words[word_i]) + 4 <= max_len:
-            line += words[word_i] + " "
-        else:
-            break
+        line = ""
+        for word_i in range(len(words)):
+            if len(line) + len(words[word_i]) + 4 <= max_len:
+                line += words[word_i] + " "
+            else:
+                break
         
-    print("*", line[:-1] + " " * (max_len - len(line) - 3), "*")
+        print("*", line[:-1] + " " * (max_len - len(line) - 3), "*")
 
-    if word_i < len(words)-1:
-        words = words[word_i:]
-        text[ind] = ' '.join(word for word in words)
-        ind -= 1
+        if word_i < len(words)-1:
+            words = words[word_i:]
+            text[ind] = ' '.join(word for word in words)
+            ind -= 1
     ind += 1
-print('*' * max_len)
+print('*' * (max_len-1) + '/')
